86e498536773305ad7a80a7594799d50f52d3c95
sheinkiruba <35525756+sheinkiruba@users.noreply.github.com>
2018-02-05 16:01:54 +0530

    natural numbers

--- shon.c
+++ shon.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 int main()
 {
-    int n, i, sum = 0;
-    
+    intk,i, sum = 0;
     printf("Enter a positive integer: ");
-    scanf("%d",&n);
-
-    for(i=1; i <= n; ++i)
+    scanf("%d",&k);
+    for(i=1; i <= k; ++i)
     {
-        sum += i;   // sum = sum+i;
+        sum += i;  
     }
-
     printf("Sum = %d",sum);
-
     return 0;
 }
