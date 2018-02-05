81b26753e767c304f80650908c195ef29ca6b9b4
sheinkiruba <35525756+sheinkiruba@users.noreply.github.com>
2018-02-05 16:08:57 +0530

    Create krish.c

---
+++ krish.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    long long k;
+    int count = 0;
+
+    printf("Enter an integer: ");
+    scanf("%d", &k);
+
+    while(k != 0)
+    {
+        k /= 10;
+        ++count;
+    }
+
+    printf("Number of digits: %d", count);
+}
