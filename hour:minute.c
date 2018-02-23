89b905f23884eab4b152c86c409433945f4c1fe9
sheinkiruba <35525756+sheinkiruba@users.noreply.github.com>
2018-02-23 16:15:37 +0530

    subtract hours and minutes

---
+++ hour:minute.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int k1,k2,k3;
+int v1,v2,v3;
+printf("Enter the time k1:v1\n");
+scanf("%d",k1);
+scanf("%d",v1);
+printf("Enter the time k2:v2\n");
+scanf("%d",k2);
+scanf("%d",v2);
+k3:v3=k1:v1-k2:v2;
+printf("%d:%d",k3,v3);
+getch();
+}
