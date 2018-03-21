1efcd74a4cf40ccc11cb94a07c10b1ca194c359c
sheinkiruba <35525756+sheinkiruba@users.noreply.github.com>
2018-03-21 16:37:06 +0530

    nearest greatest multiples of 10

---
+++ multiple.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int s,k,v,d;
+printf("Enter the number:");
+scanf("%d",&s);
+k=s/10;
+v=k+1;
+d=v*10;
+printf("The nearest greatest muliple is %d",d);
+return 0;
+}
