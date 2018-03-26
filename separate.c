bd462c4f79d4a4c4e6983094b0fdba0d0bdd9f02
sheinkiruba <35525756+sheinkiruba@users.noreply.github.com>
2018-03-26 16:31:16 +0530

    odd char & even char

---
+++ separate.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+char p[50];
+printf("enter the string:");
+scanf("%s",&p);
+printf("%c",p[0]);
+printf("%c",p[2]);
+printf("%c",p[4]);
+printf("\n %c",p[1]);
+printf("%c",p[3]);
+return 0;
+}
