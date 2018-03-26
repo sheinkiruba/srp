f4c9d21358b69729bd3314cfe17ab6254e1d353b
sheinkiruba <35525756+sheinkiruba@users.noreply.github.com>
2018-03-26 16:36:46 +0530

    area of the farm

---
+++ area.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("\n Length of farm");
+scanf("%d",&a);
+printf("\n Breath of farm");
+scanf("%d",&b);
+c=b*a;
+printf("\nArea of the farm is %d",c);
+return 0;
+}
